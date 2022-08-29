18de16c7386f1d3bf5e2484980fd0cbcbc77e921
SakshiDhote22 <110605988+SakshiDhote22@users.noreply.github.com>
2022-08-29 10:19:39 +0530

    Add files via upload

---
+++ 2ndSpringTesting/Employee.java
@@ -0,0 +1,35 @@
+package com.areyes1.jgc.unit.obj;
+
+public class Employee {
+
+	public int employeeId;
+
+	public String employeeName;
+
+	public double employeeSal;
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public double getEmployeeSal() {
+		return employeeSal;
+	}
+
+	public void setEmployeeSal(double employeeSal) {
+		this.employeeSal = employeeSal;
+	}
+
+}
